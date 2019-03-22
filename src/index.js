121cc753b3ce6b2f8ec9695096f93dcb1a99a0a9
Munis <munisjon1998@mail.ru>
2019-03-23 02:29:59 +0300

    with mistakes task advanced-zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  let zeros = 0;
+  let remainder = 1;
+  let divisor = 2;
+
+  while (base !== divisor) {
+    if (base % divisor === 0) {
+      remainder++;
+      base /= divisor;
+
+    } else {
+      remainder = 1;
+      divisor++;
+    }
+  }
+
+  while (num > 0) {
+    num = num / divisor ^ 0;
+    zeros += num;
+  }
+  return parseInt(zeros / remainder);
 }
